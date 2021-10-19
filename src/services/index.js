e5fa0af3ef2b1a377a6d54e6fcb18b98c1c6f3da
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-19 13:51:39 -0300

    feat(services): adicionando service genérica e index.

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+// const exemploServices = require('./exemploServices')
+// chamar services individuais
+
+module.exports = {
+    // exemploServices: exemploServices,
+    // exportar aqui as services
+}
---
+++ src/services/services.js
@@ -0,0 +1,31 @@
+const database = require('../models')
+
+class Services {
+  constructor(nomeDoModelo) {
+    this.nomeDoModelo = nomeDoModelo
+  }
+
+  async pegaTodosOsRegistros(where = {}) {
+    return database[this.nomeDoModelo].findAll({ where: { ...where } })
+  }
+
+  async pegaUmRegistro(where = {}) {
+    return database[this.nomeDoModelo].findOne({ where: { ...where } })
+  }
+
+  async criaRegistro(dados) {
+    return database[this.nomeDoModelo].create(dados)
+  }
+
+  async atualizaRegistro(dadosAtualizados, id){
+    return database[this.nomeDoModelo]
+      .update(dadosAtualizados, { where: { id: id } })
+  }
+
+  async apagaRegistro(id) {
+    return database[this.nomeDoModelo].destroy({ where: { id: id } })
+  }
+
+}
+
+module.exports = Services
