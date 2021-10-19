a1ab393c3dad4d1f6392b07bd26632f03e0e46ff
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-19 18:36:30 -0300

    feat(models): adicionando associações entre os models.

--- src/models/clientes.js
+++ src/models/clientes.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Clientes extends Model {
     /**
@@ -10,20 +8,41 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Clientes.belongsTo(models.Enderecos, {
+        as: "resid",
+        foreignKey: "endereco_resid",
+      });
+
+      Clientes.belongsTo(models.Enderecos, {
+        as: "comer",
+        foreignKey: "endereco_comer",
+      });
+
+      Clientes.belongsTo(models.Telefones, {
+        as: "tel",
+        foreignKey: "telefone",
+      });
+
+      Clientes.belongsTo(models.Telefones, {
+        as: "cel",
+        foreignKey: "celular",
+      });
     }
-  };
-  Clientes.init({
-    cliente_nome: DataTypes.STRING,
-    cliente_nasc: DataTypes.DATEONLY,
-    is_active: DataTypes.BOOLEAN,
-    endereco_resid: DataTypes.INTEGER,
-    endereco_comer: DataTypes.INTEGER,
-    telefone: DataTypes.INTEGER,
-    celular: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Clientes',
-  });
+  }
+  Clientes.init(
+    {
+      cliente_nome: DataTypes.STRING,
+      cliente_nasc: DataTypes.DATEONLY,
+      is_active: DataTypes.BOOLEAN,
+      endereco_resid: DataTypes.INTEGER,
+      endereco_comer: DataTypes.INTEGER,
+      telefone: DataTypes.INTEGER,
+      celular: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Clientes",
+    }
+  );
   return Clientes;
 };
--- src/models/telefones.js
+++ src/models/telefones.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Telefones.belongsTo(models.TelefonesTipos, {
+        as: "tipo_telefone",
+        foreignKey: "telefone_tipo",
+      });
     }
   };
   Telefones.init({
