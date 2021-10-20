bfb680aa449cab58193711a49e6d936184763835
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-20 09:31:09 -0300

    feat: adicionando erro genérico.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require("cors");
-const routes = require("./routes");
-const dotenv = require("dotenv");
+const cors = require('cors');
+const routes = require('./routes');
+const dotenv = require('dotenv');
 dotenv.config();
 const logger = require('./logger');
-const pinoHttp = require('pino-http')({logger});
+const pinoHttp = require('pino-http')({ logger });
 
 const app = express();
 
@@ -14,12 +14,17 @@ app.use(cors());
 app.use(routes);
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-      message: 'A api está funcionando!',
-    });
+  res.status(200).json({
+    message: 'A api está funcionando!',
+  });
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({ Erro: 'Request inválida.' });
+});
 
-app.listen(process.env.PORT, () => { console.log(`O servidor está rodando na porta ${process.env.PORT}.`) });
+app.listen(process.env.PORT, () => {
+  console.log(`O servidor está rodando na porta ${process.env.PORT}.`);
+});
 
 module.exports = app;
