b5329faf441e9b55c7b505250fc35456258c28c2
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-20 15:27:23 -0300

    feat(services): adicionando pegaUmRegistro no cliente service.

--- src/services/clientes.service.js
+++ src/services/clientes.service.js
@@ -63,6 +63,55 @@ class ClientesService extends Services {
   }
 
   /**
+   * Esta função realiza a consulta findOne no banco de dados.
+   * @param {Number} id recebe o id do objeto cliente.
+   * @param {*} dados recebe todos os dados da consulta.
+   * @returns faz o envio dos dados recebidos para o getClienteById no controller.
+   */
+  async pegaUmRegistro(id) {
+    return database[this.nomeDoModelo].findOne({
+      where: { id: Number(id) },
+      attributes: ['id', 'cliente_nome', 'is_active'],
+      include: [
+        {
+          model: Enderecos,
+          as: 'resid',
+          attributes: ['rua', 'numero', 'complemento', 'bairro', 'cidade'],
+          include: [
+            this.criaInclude(EnderecosTipos, 'tipo_endereco', 'id', 'tipo'),
+            this.criaInclude(Cidades, 'cidad', 'cidade_nome', 'cidade_uf'),
+          ],
+        },
+        {
+          model: Enderecos,
+          as: 'comer',
+          attributes: ['rua', 'numero', 'complemento', 'bairro', 'cidade'],
+          include: [
+            this.criaInclude(EnderecosTipos, 'tipo_endereco', 'id', 'tipo'),
+            this.criaInclude(Cidades, 'cidad', 'cidade_nome', 'cidade_uf'),
+          ],
+        },
+        {
+          model: Telefones,
+          as: 'tel',
+          attributes: ['ddd', 'numero'],
+          include: [
+            this.criaInclude(TelefonesTipos, 'tipo_telefone', 'id', 'tipo'),
+          ],
+        },
+        {
+          model: Telefones,
+          as: 'cel',
+          attributes: ['ddd', 'numero'],
+          include: [
+            this.criaInclude(TelefonesTipos, 'tipo_telefone', 'id', 'tipo'),
+          ],
+        },
+      ],
+    });
+  }
+
+  /**
    * Esta função cria um objeto para o include interno da consulta.
    * @param {*} model recebe o nome do modelo/tabela no banco de dados.
    * @param {string} as recebe o apelido da tabela.
