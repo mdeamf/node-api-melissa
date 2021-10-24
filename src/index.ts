acf6fc4f74fc20881d9f1651d3e6410fc2ca0639
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-24 18:38:33 -0300

    feat(logger): adicionando logger.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import { logger } from "./logger";
+import expressPinoLogger from "express-pino-logger";
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json(), cors());
+app.use(express.json());
+app.use(expressPinoLogger({ logger }));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'A api está funcionando!'});
