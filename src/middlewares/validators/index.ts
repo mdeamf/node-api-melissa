9ae405a9ff5d47d9e70e2f9ff87a3442cdb1b965
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-26 10:51:15 -0300

    feat(validators): adicionando usuarios validator.

--- src/middlewares/validators/index.ts
+++ src/middlewares/validators/index.ts
@@ -1,7 +1,9 @@
 import clientsValidator from './schemas/clients.schema.validator';
+import usuariosValidator from './schemas/usuarios.schema.validator';
 
 const schemas = {
   clientsValidator: clientsValidator,
+  usuariosValidator: usuariosValidator,
 };
 
 export default schemas;
--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -1,7 +1,13 @@
 import express, { Request, Response } from 'express';
 import UsuariosController from '../controllers/usuarios.controller';
 const router = express.Router();
+import Schemas from '../middlewares/validators';
+import Validator from '../middlewares/validators/validator.middleware';
 
-router.post('/', UsuariosController.createUsuario);
+router.post(
+  '/',
+  Validator.validator(Schemas.usuariosValidator),
+  UsuariosController.createUsuario
+);
 
 export default router;
