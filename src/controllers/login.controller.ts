711b13c8dec14fc5bb312a8cd3c41b40c342baae
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-26 10:42:22 -0300

    feat(controllers): adicionando controller de login.

---
+++ src/controllers/login.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import objetoDeResposta from '../utils/formata.objeto.de.resposta';
+import service from '../services/login.service';
+
+class LoginController {
+  static async authenticate(req: Request, res: Response) {
+    const dados = req.body;
+    try {
+      const usuario = await service.authenticate(dados);
+      const token = service.generateToken(usuario);
+      const obj = objetoDeResposta.formataObjeto(false, null, token);
+      return res.status(200).json(obj);
+    } catch (err) {
+      const obj = objetoDeResposta.formataObjeto(true, err, null);
+      return res.status(400).json(obj);
+    }
+  }
+}
+
+export default LoginController;
