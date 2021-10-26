fa9c30162c1afc254d975fbf4d7dbd93809d81d3
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-26 10:44:12 -0300

    feat(jwt): adicionando authorize mid do jwt.

---
+++ src/middlewares/jwt/authorize.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+import objetoDeResposta from '../../utils/formata.objeto.de.resposta';
+
+class jwtAuthorize {
+  static verifyToken(req: Request, res: Response, next: NextFunction) {
+    const header = req.headers.authorization || '';
+    const secret = process.env.SECRET || '';
+    try {
+      jwt.verify(header, secret, (err: any, decoded: any) => {
+        if (err) {
+          throw new Error('Não autorizado!');
+        } else {
+          next();
+        }
+      });
+    } catch (err) {
+      const obj = objetoDeResposta.formataObjeto(true, err, null);
+      return res.status(400).json(obj);
+    }
+  }
+}
+
+export default jwtAuthorize;
