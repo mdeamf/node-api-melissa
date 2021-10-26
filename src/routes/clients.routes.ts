9aa71c63c8632356d3d1c72faf85191f19ffd9d9
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-26 13:25:46 -0300

    feat: correções.

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import ClientsController from '../controllers/clients.controller';
-import Schemas from '../middlewares/validators/schemas';
-import Validator from '../middlewares/validators/validators';
+import Schemas from '../middlewares/validators/index';
+import Validator from '../middlewares/validators/validator.middleware';
 const router = express.Router();
 
 /* router.post(
