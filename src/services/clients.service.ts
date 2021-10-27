76cb3c7990a85df80f190f8fcd7dced76fd81177
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-27 16:40:52 -0300

    feat(services): adicionando createClientAddressPhone em clients service.

--- src/services/clients.service.ts
+++ src/services/clients.service.ts
@@ -2,6 +2,49 @@ import mongoose from 'mongoose';
 import { IClientBody, IClientModel } from '../interfaces/client.interface';
 import { AddressesModel, PhonesModel, ClientsModel } from '../models';
 
-class ClientsService {}
+class ClientsService {
+  static async createClientAddressPhone(clienteBody: IClientBody) {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      const clienteInserir: IClientModel = {
+        cliente_nome: clienteBody.cliente_nome,
+        cliente_nasc: clienteBody.cliente_nasc,
+        is_active: clienteBody.is_active,
+        endereco_id: [],
+        contatos: [],
+      };
+
+      const enderecos = await AddressesModel.create(clienteBody.endereco_id, {
+        session: session,
+      });
+      console.log(enderecos);
+
+      clienteInserir.endereco_id = enderecos.map((endereco) => endereco._id);
+
+      const telefones = await PhonesModel.create(clienteBody.contatos, {
+        session: session,
+      });
+      console.log(telefones);
+
+      clienteInserir.contatos = telefones.map((telefone) => telefone._id);
+
+      const cliente = await ClientsModel.create([clienteInserir], {
+        session: session,
+      });
+      console.log(cliente);
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return cliente;
+    } catch (err) {
+      await session.abortTransaction();
+      session.endSession();
+      return err;
+    }
+  }
+}
 
 export default ClientsService;
