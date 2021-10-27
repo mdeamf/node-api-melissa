69fcb4670212510ed0117f80cc31d1194e679916
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-27 16:41:48 -0300

    feat(routes): adicionando post em clients.

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express';
 import ClientsController from '../controllers/clients.controller';
-import Schemas from '../middlewares/validators/index';
+import Schemas from '../middlewares/validators';
 import Validator from '../middlewares/validators/validator.middleware';
 const router = express.Router();
+import jwt from '../middlewares/jwt/authorize.middleware';
 
-/* router.post(
-    '/',
-    Validator.validator(Schemas.clientsValidator),
-    ClientsController.createClient
-); */
+router.post(
+  '/',
+  jwt.verifyToken,
+  Validator.validator(Schemas.clientsValidator),
+  ClientsController.createClientAndAddressPhone
+);
 
 export default router;
