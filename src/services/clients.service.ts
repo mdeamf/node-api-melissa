d8d0c73caae218a990d933f6ad5215f9406f6af8
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-27 16:35:16 -0300

    feat(services): adicionando clients service.

---
+++ src/services/clients.service.ts
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+import { IClientBody, IClientModel } from '../interfaces/client.interface';
+import { AddressesModel, PhonesModel, ClientsModel } from '../models';
+
+class ClientsService {}
+
+export default ClientsService;
