a99ed733502b2093c9c0e42caefaa7c7297c9347
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-27 16:42:34 -0300

    feat(controllers): adicionando createClientAndAddressPhone em clients.

--- src/controllers/clients.controller.ts
+++ src/controllers/clients.controller.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from 'express';
 import objetoDeResposta from '../utils/formata.objeto.de.resposta';
+import service from '../services/clients.service';
 
 class ClientsController {
-  /* static async createClient(req: Request, res: Response){
-      const data = req.body
-      try {
-        const obj = objetoDeResposta.formataObjeto(false, null, data);
-        return res.status(200).json(obj);
-      } catch (err) {
-        const obj = objetoDeResposta.formataObjeto(true, err, null);
-        return res.status(400).json(obj);
-      }
-    }  */
+  static async createClientAndAddressPhone(req: Request, res: Response) {
+    try {
+      const cliente = await service.createClientAddressPhone(req.body);
+      const obj = objetoDeResposta.formataObjeto(false, null, cliente);
+      return res.status(200).json(obj);
+    } catch (err) {
+      const obj = objetoDeResposta.formataObjeto(true, err, null);
+      return res.status(400).json(obj);
+    }
+  }
 }
 
 export default ClientsController;
