a19a0f5804bfdf6527d3ac7ebeced75b8327f8b2
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-27 15:40:17 -0300

    feat(models): adicionando índice.

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import Clients from '../models/clients.model';
+import Addresses from './addresses.model';
+import Phones from './phones.model';
+import Usuarios from './usuarios.model';
+
+export const ClientsModel = Clients;
+export const AddressesModel = Addresses;
+export const PhonesModel = Phones;
+export const UsuariosModel = Usuarios;
